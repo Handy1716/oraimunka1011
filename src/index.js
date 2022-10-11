be409b3f483ed8c21d2773827482fba3113b7d47
Polgár Kristóf <polgarkristof10@gmail.com>
2022-10-11 12:30:12 +0200

    dasda

--- src/index.js
+++ src/index.js
@@ -7,9 +7,19 @@ function getRandomInt(min, max) {
 }
 
 let tomb = [];
+let ottelOszthato=[];
 document.addEventListener('DOMContentLoaded', () => {
     for( var i = 0; i<20; i++) {
         tomb.push(getRandomInt(1,30));
+        if(tomb[i]%5===0) { ottelOszthato.push(tomb[i])};
         console.log(tomb[i]);
     }
+    console.log('');
+    for( var i = 0; i<20; i++) {
+        console.log(ottelOszthato[i]);
+    }
+    document.getElementById('meghivas').addEventListener("click", () => {
+        ottelOszthato = tomb.filter(e => (e%5)==0);
+        console.log(ottelOszthato);
+    });
 });
